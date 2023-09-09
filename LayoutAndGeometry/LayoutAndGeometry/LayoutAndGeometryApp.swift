c8670313c083e0715348d6e07690f0728b688ab3
mapledeveh <mapledeveh@gmail.com>
2023-09-09 17:50:21 -0400

    Update Progress

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Alex Nguyen on 2023-09-09.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
