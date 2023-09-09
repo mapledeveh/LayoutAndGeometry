a078327d3e91a5298e85784ba7df4f57b41b456b
mapledeveh <mapledeveh@gmail.com>
2023-09-09 17:50:46 -0400

    Update Progress

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,15 +7,43 @@
 
 import SwiftUI
 
+extension VerticalAlignment {
+//    struct MidAccountAndName: AlignmentID {
+    enum MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+    
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@mapledeveh")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                Image("alex-nguyen")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+//                Text("Full name:")
+//                Text("Full name:")
+//                Text("Full name:")
+//                Text("Full name:")
+//                Text("Full name:")
+            }
+            
+            VStack {
+//                Text("Full name:")
+//                Text("Full name:")
+//                Text("Full name:")
+//                Text("Full name:")
+                Text("ALEX NGUYEN")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                    .font(.largeTitle)
+            }
         }
-        .padding()
     }
 }
 
